c7ca182b29c3bc3e75e6b3479bf845e8dcd0dbe9
Ajie Raharjo <ajiekurnia@hotmail.com>
2023-07-09 13:15:18 +0700

    Update wms-deployment-aws-eb.sh
    sudo rm -f lifecycle.json
    echo '{' >> lifecycle.json
    echo '    "Rules": [' >> lifecycle.json
    echo '        {' >> lifecycle.json
    echo '            "ID": "delete-after-2-days",' >> lifecycle.json
    echo '            "Status": "Enabled",' >> lifecycle.json
    echo '            "Prefix": "logs/",' >> lifecycle.json
    echo '            "NoncurrentVersionExpiration": {' >> lifecycle.json
    echo '                "NoncurrentDays": 2' >> lifecycle.json
    echo '            }' >> lifecycle.json
    echo '        }' >> lifecycle.json
    echo '    ]' >> lifecycle.json
    echo '}' >> lifecycle.json
    aws s3api put-bucket-lifecycle --bucket $WMS_S3_BUCKET --lifecycle-configuration file://lifecycle.json

--- wms-deployment-aws-eb.sh
+++ wms-deployment-aws-eb.sh
@@ -135,18 +135,18 @@ sudo rm -f ~/wms-backend-v1.0.0.zip
 sudo rm -f ~/wms.zip
 #------------------------------ Create JSON for S3 Permission LifeCyle auto delete after 2 days
 sudo rm -f lifecycle.json
-echo '{\n' >> lifecycle.json
-echo '    "Rules": [\n' >> lifecycle.json
-echo '        {\n' >> lifecycle.json
-echo '            "ID": "delete-after-2-days",\n' >> lifecycle.json
-echo '            "Status": "Enabled",\n' >> lifecycle.json
-echo '            "Prefix": "logs/",\n' >> lifecycle.json
-echo '            "NoncurrentVersionExpiration": {\n' >> lifecycle.json
-echo '                "NoncurrentDays": 2\n' >> lifecycle.json
-echo '            }\n' >> lifecycle.json
-echo '        }\n' >> lifecycle.json
-echo '    ]\n' >> lifecycle.json
-echo '}\n' >> lifecycle.json
+echo '{' >> lifecycle.json
+echo '    "Rules": [' >> lifecycle.json
+echo '        {' >> lifecycle.json
+echo '            "ID": "delete-after-2-days",' >> lifecycle.json
+echo '            "Status": "Enabled",' >> lifecycle.json
+echo '            "Prefix": "logs/",' >> lifecycle.json
+echo '            "NoncurrentVersionExpiration": {' >> lifecycle.json
+echo '                "NoncurrentDays": 2' >> lifecycle.json
+echo '            }' >> lifecycle.json
+echo '        }' >> lifecycle.json
+echo '    ]' >> lifecycle.json
+echo '}' >> lifecycle.json
 aws s3api put-bucket-lifecycle --bucket $WMS_S3_BUCKET --lifecycle-configuration file://lifecycle.json
 rm -f ~/lifecycle.json
 #------------------------------ S3 Bucket name information for user
