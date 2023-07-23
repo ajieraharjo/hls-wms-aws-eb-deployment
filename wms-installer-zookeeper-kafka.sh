edadd5ed2ec23240a4816accb2f199ede22136ea
Ajie Raharjo <ajiekurnia@hotmail.com>
2023-07-23 10:14:44 +0700

    First release v1.0.0 (tested on ubuntu 22)

--- wms-installer-zookeeper-kafka.sh
+++ wms-installer-zookeeper-kafka.sh
@@ -7,7 +7,7 @@ sudo mkdir /data/zk-dataDir
 sudo wget https://dlcdn.apache.org/zookeeper/zookeeper-3.7.1/apache-zookeeper-3.7.1-bin.tar.gz -O /data/software/apache-zookeeper-3.7.1-bin.tar.gz
 sudo tar -zxvf /data/software/apache-zookeeper-3.7.1-bin.tar.gz -C /data
 sudo cp /data/apache-zookeeper-3.7.1-bin/conf/zoo_sample.cfg /data/apache-zookeeper-3.7.1-bin/conf/zoo.cfg
-#---------------------- zookeeper configuration
+#---------------------- ZOOKEEPER configuration
 sudo sed -i '$a tickTime=2000' /data/apache-zookeeper-3.7.1-bin/conf/zoo.cfg
 sudo sed -i '$a initLimit=10' /data/apache-zookeeper-3.7.1-bin/conf/zoo.cfg
 sudo sed -i '$a syncLimit=5' /data/apache-zookeeper-3.7.1-bin/conf/zoo.cfg
@@ -17,14 +17,13 @@ sudo sed -i '$a clientPort=62181' /data/apache-zookeeper-3.7.1-bin/conf/zoo.cfg
 sudo sed -i '$a maxClientCnxns=200' /data/apache-zookeeper-3.7.1-bin/conf/zoo.cfg
 sudo sed -i '$a autopurge.snapRetainCount=3' /data/apache-zookeeper-3.7.1-bin/conf/zoo.cfg
 sudo sed -i '$a autopurge.purgeInterval=2' /data/apache-zookeeper-3.7.1-bin/conf/zoo.cfg
-#---------------------- add Log directory into zookeeper zkEnv.sh
+#---------------------- add Log directory into ZOOKEEPER zkEnv.sh
 sudo sed -i 's/\$ZOOKEEPER_PREFIX\/logs/\/data\/zk-dataLogDir/g' /data/apache-zookeeper-3.7.1-bin/bin/zkEnv.sh
 #---------------------- KAFKA Installation
 sudo mkdir /data/kafka-logs
 sudo wget https://archive.apache.org/dist/kafka/2.2.1/kafka_2.11-2.2.1.tgz -O /data/software/kafka_2.11-2.2.1.tgz
 sudo tar -zxvf /data/software/kafka_2.11-2.2.1.tgz -C /data
 #---------------------- KAFKA configuration server.properties
-#advertised.listeners=PLAINTEXT://your.host.name:9092
 WMS_KAFKA=127.0.0.1:9092
 WMS_KAFKA_LOG=/data/kafka-logs
 WMS_KAFKA_LOG="${WMS_KAFKA_LOG//\//\\/}"
@@ -32,11 +31,6 @@ WMS_KAFKA_ZOOKEEPER=127.0.0.1:62181
 sudo sed -i 's/\(#advertised.listeners=\)/advertised.listeners=PLAINTEXT:\/\/'$WMS_KAFKA'\n\1/g' /data/kafka_2.11-2.2.1/config/server.properties
 sudo sed -i 's/\(log.dirs=\)/log.dirs='$WMS_KAFKA_LOG' \n\#\1/g' /data/kafka_2.11-2.2.1/config/server.properties 
 sudo sed -i 's/\(zookeeper.connect=\)/zookeeper.connect='$WMS_KAFKA_ZOOKEEPER' \n\#\1/g' /data/kafka_2.11-2.2.1/config/server.properties
-#sudo sed -i 's/\(log.dirs=\)/log.dirs=/'$WMS_KAFKA_LOG' \1/g' /data/kafka_2.11-2.2.1/config/server.properties
-#log.dirs=/tmp/kafka-logs
-#zookeeper.connect=localhost:2181
-#sudo sed -i 's/\(#advertised.listeners=\)/advertised.listeners=PLAINTEXT://your.host.name:9092\1/g' /data/kafka_2.11-2.2.1/config/server.properties
-#sed 's/\(#advertised.listeners=\)/'$WMS_KAFKA'\1/g' /data/kafka_2.11-2.2.1/config/server.properties
-#sed -i  's/\(<Valve className="org.apache.catalina.valves.RemoteAddrValve"\)/<!--\1/g' apache-tomcat-9.0.14/webapps/manager/META-INF/context.xml 
+#---------------------- Start ZOOKEEPER and KAFKA 
 sudo /data/apache-zookeeper-3.7.1-bin/bin/zkServer.sh start
 sudo /data/kafka_2.11-2.2.1/bin/kafka-server-start.sh /data/kafka_2.11-2.2.1/config/server.properties
